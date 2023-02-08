98a5b54124a63f6526d8354e225c85de5925a46b
OmarAdamMostafa <115920338+OmarAdamMostafa@users.noreply.github.com>
2023-02-08 15:54:19 +0200

    Updated Deployment Issue

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -2,7 +2,7 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import { v2 as cloudinary } from 'cloudinary';
 
-import Post from '../mongodb/models/post.js';
+import post from '../mongodb/models/post.js';
 
 dotenv.config();
 
@@ -16,7 +16,7 @@ cloudinary.config({
 
 router.route('/').get(async (req, res) => {
   try {
-    const posts = await Post.find({});
+    const posts = await post.find({});
     res.status(200).json({ success: true, data: posts });
   } catch (err) {
     res.status(500).json({ success: false, message: 'Fetching posts failed, please try again' });
@@ -28,7 +28,7 @@ router.route('/').post(async (req, res) => {
     const { name, prompt, photo } = req.body;
     const photoUrl = await cloudinary.uploader.upload(photo);
 
-    const newPost = await Post.create({
+    const newPost = await post.create({
       name,
       prompt,
       photo: photoUrl.url,
